827f144fe7f5a8af1e57c0f3240dafba420d475a
Christine <cmelasigue@gmail.com>
2016-01-21 01:35:59 +0000

    Lesson 5 Setting up the Database Take 2

--- db/migrate/20160121012500_create_quotes.rb
+++ db/migrate/20160121012500_create_quotes.rb
@@ -2,8 +2,8 @@ class CreateQuotes < ActiveRecord::Migration
   def change
     create_table :quotes do |t|
 
-    	t string :saying
-    	t string :author
+    	t.string :saying
+    	t.string :author
 
       t.timestamps
     end
